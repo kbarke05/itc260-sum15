285c15aab28e03e0f23297f8a0c87eaadc8d4725
Kristie Barker <kbarke05>
2015-07-16 15:42:33 -0700

    Finishes add form and adds Rss start

--- application/config/routes.php
+++ application/config/routes.php
@@ -64,9 +64,10 @@ $route['translate_uri_dashes'] = FALSE;
 //$route['default_controller'] = 'pages/view';
 
 //$route['(:any)'] = 'pages/view/$1';
-
+$route['news/create'] = 'news/create';
 $route['news/(:any)'] = 'news/view/$1';
 $route['news'] = 'news';
+$route['rss'] = 'rss';
 $route['(:any)'] = 'pages/view/$1';
 $route['default_controller'] = 'pages/view';
 
---
+++ application/controllers/Rss.php
@@ -0,0 +1,34 @@
+<?php
+//Rss.php
+class Rss extends CI_Controller {
+
+
+		public function index()
+		{
+				
+			//$request = "http://rss.news.yahoo.com/rss/software";
+			//$request = "https://news.google.com/news?pz=1&cf=all&ned=us&hl=en&topic=e&output=rss";
+			$request = "https://www.google.com/search?hl=en&gl=us&tbm=nws&authuser=0&q=lebron+james&oq=lebron+james&gs_l=news-cc.3..43j0l9j43i53.214462.218028.0.218630.12.6.0.6.6.0.188.815.2j4.6.0...0.0...1ac.1.BU39atpDqmo";
+			$response = file_get_contents($request);
+			$xml = simplexml_load_string($response);
+			  print '<h1>' . $xml->channel->title . '</h1>';
+			  foreach($xml->channel->item as $story)
+			  {
+				echo '<a href="' . $story->link . '">' . $story->title . '</a><br />'; 
+				echo '<p>' . $story->description . '</p><br /><br />';
+			  }
+				
+				
+				
+				
+				//$data['news'] = $this->news_model->get_news();
+				//$data['title'] = 'News archive';
+
+				//$this->load->view('templates/header', $data);
+				//$this->load->view('news/index', $data);
+				//$this->load->view('templates/footer');
+		}//end index()
+
+		
+        
+}//end Rss
